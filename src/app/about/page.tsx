628e6aa3e2d8cad103ac30f91a90726bbf3aed9e
Big_Carp <tobilobad1738@gmail.com>
2025-08-26 12:27:40 +0100

    fix: responsiveness of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,7 +15,7 @@ const About = () => {
         transition={{ duration: 0.8 }}
         viewport={{ once: true }}
       >
-        <div className="max-w-7xl mx-auto px-4 py-10 md:bg-[url(/rocket.png)] bg-no-repeat bg-top-right bg-size-contain">
+        <div className="max-w-6xl mx-auto px-4 py-10 md:bg-[url(/rocket.png)] bg-no-repeat bg-top-right bg-size-contain">
           <motion.h1
             className="text-[20px] md:text-[25px] text-[#003399] font-bold text-center"
             initial={{ opacity: 0 }}
@@ -42,7 +42,7 @@ const About = () => {
           </motion.p>
         </div>
         <motion.div
-          className="max-w-7xl mx-auto mb-30"
+          className="max-w-6xl mx-auto mb-30"
           initial={{ opacity: 0, scale: 0.9 }}
           whileInView={{ opacity: 1, scale: 1 }}
           transition={{ delay: 0.8 }}
@@ -59,7 +59,7 @@ const About = () => {
 
       {/* Vision and Mission Section */}
       <motion.div
-        className="max-w-7xl mx-auto"
+        className="max-w-6xl mx-auto"
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.8 }}
@@ -84,16 +84,23 @@ const About = () => {
             />
           </motion.div>
           <motion.div
-            className="w-98% md:w-1/2 p-2 md:p-0 flex flex-col justify-start items-center"
+            className="w-98% md:w-1/2 p-2 md:p-0 f"
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 0.8 }}
           >
-            <p className="text-[25px] md:text-[35px] font-bold text-[#003399]">
-              To provide user-friendly experience for users
-            </p>
-            <p className="flex justify-center items-center mt-4 text-[16px] md:text-[20px]">
-              Kript is a cutting-edge cryptocurrency trading platform that seamlessly combines innovation with accessibility. Whether you&apos;re a seasoned crypto enthusiast or a newcomer to the digital asset space, Kript offers a user-friendly experience for buying, selling, and holding a diverse range of cryptocurrencies.
+            {/* <p className="text-[25px] md:text-[35px] font-bold text-[#003399]">
+            FREEDOM TO USE YOUR MONEY WHENEVER YoU WANT. 
+            </p> */}
+            <p className="mt-4 text-[16px] md:text-[20px]">
+              <span className="text-[25px] md:text-[35px] font-bold text-[#003399]">Our mission</span> is to give 
+              people the freedom to use their money whenever they want, without unnecessary
+              restrictions or delays. We envision a world where financial control is fully in your
+              hands—where you can access, spend, and move your money freely, at any time, and for any purpose.
+              At the same time, we aim to give you the freedom of crypto without borders, empowering you to explore,
+              transact, and interact with digital assets globally. We want to remove barriers, simplify access, and
+              ensure that everyone can experience the true potential of decentralized finance, enabling a future where
+              your money works for you, anytime, anywhere, and without limits.
             </p>
           </motion.div>
         </div>
@@ -119,11 +126,17 @@ const About = () => {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 0.8 }}
           >
-            <p className="text-[25px] md:text-[35px] font-bold text-[#003399]">
+            {/* <p className="text-[25px] md:text-[35px] font-bold text-[#003399]">
               Seamless Crypto Trading Experience
-            </p>
-            <p className="flex justify-center items-center mt-4 text-[16px] md:text-[20px]">
-              Kript is a modern cryptocurrency platform built to simplify trading and enhance accessibility. Designed for both crypto veterans and newcomers, Kript delivers an easy-to-use interface for buying, selling, and managing a diverse selection of digital assets effortlessly.
+            </p> */}
+            <p className="mt-4 text-[16px] md:text-[20px]">
+              <span className="text-[25px] md:text-[35px] font-bold text-[#003399]">Our vision</span> is to make 
+              crypto simple, reliable, and accessible for everyone, regardless of experience or background.
+              We aim to build a seamless, fully automated platform that removes complexity, eliminates unnecessary barriers, and
+              empowers users to confidently manage their finances and digital assets. By bridging the gap between traditional finance
+              and the evolving world of crypto, we strive to provide tools and solutions that make digital currencies practical,
+              secure, and usable in everyday life. Our goal is to enable individuals to explore new financial opportunities, transact globally
+              without friction, and embrace the freedom and potential that a borderless financial system offers.
             </p>
           </motion.div>
         </div>
